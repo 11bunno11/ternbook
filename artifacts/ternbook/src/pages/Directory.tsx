714918623a22c790a8593ffbfb4927077883751a
11bunno11 <112693429+11bunno11@users.noreply.github.com>
2026-06-01 04:24:24 +0000

    made it optional chaining on displayedSites.map

--- artifacts/ternbook/src/pages/Directory.tsx
+++ artifacts/ternbook/src/pages/Directory.tsx
@@ -269,7 +269,7 @@ export default function Directory() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-        {displayedSites.map((site) => (
+        {displayedSites?.map((site) => (
           <SiteCard key={site.url} site={site} />
         ))}
       </div>
